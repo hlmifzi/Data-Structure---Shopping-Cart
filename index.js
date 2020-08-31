ff6a1c30246a281563af9ee4cf9b3cea00d1d15d
Helmi <hlmifzi@gmail.com>
2020-09-01 00:08:31 +0700

    remove testing chek parameter type

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ keranjang.tambahProduk("Semangka Kuning", 3);
 keranjang.tambahProduk("Apel Merah", 1);
 keranjang.tambahProduk("Apel Merah", 4);
 keranjang.tambahProduk("Apel Merah", 2);
-keranjang.hapusProduk()
+
 keranjang.hapusProduk("Semangka Kuning");
 keranjang.hapusProduk("Semangka Merah");
 keranjang.tampilkanCart();
